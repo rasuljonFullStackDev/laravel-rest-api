a7f1ebb2fd646d10caf962625098d2cafb8a7817
Abdurasul <91907630+rasuljonFullStackDev@users.noreply.github.com>
2024-05-25 11:04:49 +0500

    Update api.php
    route

--- routes/api.php
+++ routes/api.php
@@ -27,12 +27,12 @@ Route::post('login',[AuthController::class,'login']);
 Route::post('cars/add',[CarsController::class,'add']);
 Route::get('cars',[CarsController::class,'get']);
 Route::get('cars/{id}',[CarsController::class,'get']);
-Route::post('cars/edit/{id}',[CarsController::class,'edit']);
-Route::post('cars/delete/{id}',[CarsController::class,'delete']);
+Route::put('cars/edit/{id}',[CarsController::class,'edit']);
+Route::delete('cars/delete/{id}',[CarsController::class,'delete']);
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('user',[AuthController::class,'user']);
-    Route::post('user/delete',[AuthController::class,'deleteaccount']);
-    Route::post('user/edit/',[AuthController::class,'useredit']);
+    Route::delete('user/delete',[AuthController::class,'deleteaccount']);
+    Route::put('user/edit/',[AuthController::class,'useredit']);
     Route::post('user/password-change/',[AuthController::class,'passwordChange']);
     Route::post('logout',[AuthController::class,'logout']);
 });
